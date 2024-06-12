f68d846d5c3da4541eda19786af091fd52254883
Mohamed-Gamal00 <gmohamed777@yahoo.com>
2024-06-12 17:50:02 +0300

    last update

--- resources/views/dashboard/layouts/partials/footer.blade.php
+++ resources/views/dashboard/layouts/partials/footer.blade.php
@@ -1,7 +1,6 @@
 <!-- Footer opened -->
 	<div class="main-footer ht-40">
 		<div class="container-fluid text-center pd-t-0-f ht-100p">
-			<span>Copyright © 2020 <a href="#">lawfirm</a>. Designed by <a href="https://github.com/Mohamed-Gamal00/BloodBank">desinger</a> All rights reserved.</span>
 		</div>
 	</div>
 <!-- Footer closed -->
